a9a366bba004380632dce521f51b4424bc5649d5
konradgorni <gornikonrad@gmail.com>
2020-06-15 17:48:10 +0200

    sematical changes in html,use 1 function instead 2.

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const hamburger = document.getElementById('hamburger');
-let nav = document.getElementById('nav');
-let x = document.getElementById('x');
-let bars = document.getElementById('bars');
+const nav = document.getElementById('nav');
+const x = document.getElementById('x');
+const bars = document.getElementById('bars');
 
 function toggleMenu() {
   nav.classList.toggle('active');
-  console.log(innerHeight);
   x.classList.toggle('xactive');
   x.style.color = 'white';
   bars.classList.toggle('hamburgerActive');
 }
 hamburger.addEventListener('click', toggleMenu, false);
-
-const links = document.getElementsByClassName('header__nav__list__item');
-function closeMenuByLink() {
-  nav.classList.toggle('active');
-  x.classList.toggle('xactive');
-  bars.classList.toggle('hamburgerActive');
-}
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', closeMenuByLink, false);
-}
+const links = document.querySelectorAll('.header__nav__list__item');
+links.forEach((element) =>
+  element.addEventListener('click', toggleMenu, false)
+);
